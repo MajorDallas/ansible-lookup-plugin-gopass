90bf197579b424b7a6d882ae403c6349f22bde99
MajorDallas <79329882+MajorDallas@users.noreply.github.com>
2022-12-01 18:13:43 -0500

    Update gopass.py
    Adds `-o` (Display only the password) option to prevent trailing newlines.

--- lookup_plugins/gopass.py
+++ lookup_plugins/gopass.py
@@ -31,7 +31,7 @@ class LookupModule(LookupBase):
     def run(self, terms, variables=None, **kwargs):
         res = []
         display.debug("gopass lookup secret: %s" % terms[0])
-        result = subprocess.run(["gopass", "show", "%s" % (terms[0])], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        result = subprocess.run(["gopass", "show", "-o", "%s" % (terms[0])], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
         display.vvvv(u"gopass lookup secret at %s" % result)
         try:
             if result.returncode == 0:
